83faa0071fd00da266b50c94c7dd2a55a1a71457
Claire <97535035+CLg003@users.noreply.github.com>
2022-08-10 21:33:07 +0100

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import {useState, useEffect} from 'react';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div id="App">
+      <header>
+        <h1>Uno</h1>
+      </header>
+      {/* MAIN CONTENT HERE */}
+      <footer>
+        <p>footer here</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
