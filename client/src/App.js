acd5d572112d1b2c0f536722b862ca0881931ffd
Claire <97535035+CLg003@users.noreply.github.com>
2022-08-11 12:06:41 +0100

    Tested client-server link up.

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,25 @@
 import './App.css';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import Header from './components/Header';
 import EnterUserName from './containers/EnterUserName';
 import UnoGame from './containers/UnoGame';
+import { getCards } from './services/CardsService';
 
 function App() {
 
     const [user, setUser] = useState(null);
+    const [cards, setCards] = useState(null);
 
     const inputUserName = (name) => {
-      setUser(name);
+        setUser(name);
     }
+
+    useEffect(() => {
+        getCards()
+        .then((data) => {
+            setCards(data);
+        });
+    }, []);
 
     return (
         <div id="app">
@@ -19,7 +28,7 @@ function App() {
             { !user ?
             <EnterUserName inputUserName={inputUserName}/>
             :
-            <UnoGame/>
+            <UnoGame cards={cards} user={user}/>
             }
             <footer>&copy;2022 Claire Laing</footer>
         </div>
--- client/src/containers/UnoGame.js
+++ client/src/containers/UnoGame.js
@@ -1,5 +1,20 @@
 
-const UnoGame = () => {
+const UnoGame = ({cards, user}) => {
+
+    if(cards) {
+        return (
+            <div id="uno-game-container">
+                <p>Uno card game here</p>
+                {/* Need 3 areas to drag items from - player 1, player 2 and pick-up pile*/}
+                {/* Need 3 dropzones - player 1, player 2, play/discard pile */}
+                {/* Acceptable types should be equal to current discard pile colour/symbol/wild */}
+                {/* States for player 1, player 2 (both 7 cards on dealing from shuffled/random-sorted cards), pick-up pile (top card=[0], .shift from start of array), discard pile (top card=[0], .unshift into start of array) */}
+                {/* Randomise order of cards then deal 7 to each of player 1 and player 2, 
+                remaining cards to pick-up pile, then top card [0] shift/unshift to play/discard pile (top card) */}
+                <p>card access test - First card in pile is "{cards[0].colour}, {cards[0].symbol}"</p>
+            </div>
+        );
+    }
 
 }
 
